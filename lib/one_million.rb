1b6a7fd916f654a1e531544376d5f5e757d9f1f1
Sroop Sunar <sroop@users.noreply.github.com>
2014-07-09 11:19:01 +0100

    added single digits into a hash to pass the two tests so far

--- lib/one_million.rb
+++ lib/one_million.rb
@@ -1,4 +1,16 @@
 
+WORDS = {
+  1 => 'one',
+  2 => 'two',
+  3 => 'three',
+  4 => 'four',
+  5 => 'five',
+  6 => 'six',
+  7 => 'seven',
+  8 => 'eight',
+  9 => 'nine'
+}
+
 def number_to_words(number)
-  "one"
+  WORDS[number]
 end
