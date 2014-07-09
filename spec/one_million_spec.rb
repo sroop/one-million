2bf3ad16f55d42811d76df6c9e1da183337a31ad
Sroop Sunar <sroop@users.noreply.github.com>
2014-07-09 11:15:43 +0100

    testing for second single digit number

--- spec/one_million_spec.rb
+++ spec/one_million_spec.rb
@@ -5,7 +5,11 @@ describe 'Prints one to a million in words' do
   context '#number_to_words' do
 
     it 'converts 1 to one' do
-      expect(number_to_words(1)).to eq('one')
+      expect(number_to_words(1)).to eq('one') 
+    end
+
+    it 'converts 2 to two' do
+      expect(number_to_words(2)).to eq('two')
     end
 
   end
