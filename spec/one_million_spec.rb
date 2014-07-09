1c636497237d23678cad561b72422b15974096c7
Sroop Sunar <sroop@users.noreply.github.com>
2014-07-09 11:45:12 +0100

    tested for tens, still passing as still deciding what has to go into the master hash, no logic in there yet

--- spec/one_million_spec.rb
+++ spec/one_million_spec.rb
@@ -4,12 +4,31 @@ describe 'Prints one to a million in words' do
 
   context '#number_to_words' do
 
-    it 'converts 1 to one' do
-      expect(number_to_words(1)).to eq('one') 
+    context '1..19' do
+
+      it 'converts 1 to one' do
+        expect(number_to_words(1)).to eq('one') 
+      end
+
+      it 'converts 2 to two' do
+        expect(number_to_words(2)).to eq('two')
+      end
+
+      it 'converts 13 to thirteen' do
+        expect(number_to_words(13)).to eq('thirteen')
+      end
     end
 
-    it 'converts 2 to two' do
-      expect(number_to_words(2)).to eq('two')
+    context '20, 30, 40...90' do
+
+      it 'convert 20 to twenty' do
+        expect(number_to_words(20)).to eq('twenty')
+      end
+
+      it 'convert 90 to ninety' do
+        expect(number_to_words(90)).to eq('ninety')
+      end
+
     end
 
   end
